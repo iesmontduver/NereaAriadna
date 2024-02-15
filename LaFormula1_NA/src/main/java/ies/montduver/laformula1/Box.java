f527ffe41c03d40f24ca655e465c7f80c2cae280
ariferalv2 <ariferalv2@INF1-PC04>
2024-02-15 09:27:16 +0100

    n

--- LaFormula1_NA/src/main/java/ies/montduver/laformula1/Box.java
+++ LaFormula1_NA/src/main/java/ies/montduver/laformula1/Box.java
@@ -13,19 +13,14 @@ public class Box {
     private int numMecanics;
     
     
+   
     
-    //TO DO: crear el constructor, getters i setters que s’observa al diagrama de classe.
-    
-    //ATENCIO: Una vegada faces els canvis, descomenta el mètode següent per a que el projecte funcione */
-    
-    
-    public double realitzarPitStop(Cotxe c){
-        double ps = ((Math.random()*3 + 2 ) * 100.0) / 100.0;
+        public double realitzarPitStop(Cotxe c) {
+        double ps = ((Math.random() * 3 + 2) * 100.0) / 100.0;
         c.getRodes().setTipo_neumatic((int) (Math.random() * 4) + 1);
-        System.out.println("El " + c.getEscuderia().getNom() + " conduït per " + c.getPilot().getNom() + " ha fet el pit stop en " + ((int) (ps * 100.0))/100.0 + " segons. Ha montat rodes "+ c.getRodes().getTipo_neumatic());
+        System.out.println("El " + c.getEscuderia().getNom() + " conduït per " + c.getPilot().getNom() + " ha fet el pit stop en " + ((int) (ps * 100.0)) / 100.0 + " segons. Ha montat rodes " + c.getRodes().getTipo_neumatic());
         return ps; // tornarà entre 2 i 5
     }
-   
 
     public Box(Escuderia escuderia, int numMecanics) {
         this.escuderia = escuderia;
@@ -47,5 +42,5 @@ public class Box {
     public int getNumMecanics() {
         return numMecanics;
     }
-    
+
 }
